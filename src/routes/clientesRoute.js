c886918aa822532e9b54e155539fe5c461da231b
Elder Schmoeller <elderschmoellerls@gmail.com>
2021-12-01 20:45:57 -0300

    FIX: Arrumando sintaxe no local Routes

--- src/routes/clientesRoute.js
+++ src/routes/clientesRoute.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/clientes', ClientesController.index);
 router.get('/clientes/:id', ClientesController.show);
 router.post('/clientes', ClientesController.save);
-router.put('/clientes:id', ClientesController.update);
-router.delete('/clientes:id', ClientesController.remove);
+router.put('/clientes/:id', ClientesController.update);
+router.delete('/clientes/:id', ClientesController.remove);
 
 module.exports = router;
